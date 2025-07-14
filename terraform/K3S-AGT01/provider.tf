1bface5b8e393df2f08bb9ab474b0ae4d1ce446b
Justin De Sio <desio.j@live.fr>
2025-07-14 21:25:46 +0200

    feat(IaC) init

---
+++ terraform/K3S-AGT01/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.2-rc01"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://p1.justindesio.com:8006/api2/json"
+}
